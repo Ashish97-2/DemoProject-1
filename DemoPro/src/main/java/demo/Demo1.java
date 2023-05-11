993c4d7ee00cd7cbb5655f979856b3a392f1098f
CHARAN <CHARAN@CHARAN>
2023-05-11 12:24:02 +0530

    Engg1 second commit

--- DemoPro/src/main/java/demo/Demo1.java
+++ DemoPro/src/main/java/demo/Demo1.java
@@ -3,5 +3,7 @@ package demo;
 public class Demo1 {
 public static void main(String[] args) {
 	System.out.println("FrameWork created");
+	System.out.println("FrameWork Updated");
+	System.out.println("FrameWork viewed");
 }
 }
